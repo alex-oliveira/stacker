8e048639e807b306374efb074369755e0f222439
Alex Oliveira <bsi.alexoliveira@gmail.com>
2019-12-14 19:26:40 -0300

    no message

--- init.sh
+++ init.sh
@@ -11,6 +11,19 @@
 STACK=$1
 APP_ROOT=$(pwd)
 
+cp ../../stacks/$STACK/.env .
 cp ../../stacks/$STACK/docker-compose.yml .
 
-sed -i 's/__APP_ROOT__/${APP_ROOT}/g' ${APP_ROOT}/docker-compose.yml
+sed -i 's/__APP_ROOT__/${APP_ROOT}/g' ${APP_ROOT}/.env
+
+mkdir -p ${APP_ROOT}/data/database
+mkdir -p ${APP_ROOT}/data/storage
+mkdir -p ${APP_ROOT}/logs/nginx
+mkdir -p ${APP_ROOT}/logs/database
+mkdir -p ${APP_ROOT}/www
+
+touch ${APP_ROOT}/www/index.html
+echo "works!" > ${APP_ROOT}/www/index.html
+
+#docker-compose build
+#docker-compose up -d
